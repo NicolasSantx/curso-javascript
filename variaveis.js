96925d791ce6ad9ff6f70fe62a2dae8fcf14acd3
NicolasSantx <rm99739@estudante.fieb.edu.br>
2026-04-09 09:08:28 -0300

    javascript

---
+++ variaveis.js
@@ -0,0 +1,13 @@
+//Váriaveis em Javascript 
+//Váriaveis global
+var nome = 'Nicolas';
+// váriavel local
+let numero = 49;
+//constante (valor fixo)
+const PI = 3.14;
+
+console.log(nome);
+console.log(numero);
+console.log(PI);
+
+
